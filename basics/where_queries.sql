49c674a62f2dfd5542b8cac76180eb9a7d556470
Sammy Bruinsmsa <bruinsma.sammy@gmail.com>
2026-03-17 12:59:12 +0100

    Update where_queries.sql

--- basics/where_queries.sql
+++ basics/where_queries.sql
@@ -29,3 +29,21 @@ SELECT name
 ,population
 FROM country
 WHERE population BETWEEN 100000 AND 300000;
+
+-- OR conditions
+SELECT name
+,continent
+FROM country
+WHERE continent= 'Europe' OR continent= 'Asia';
+
+-- Check for NULL values
+SELECT name
+,region
+FROM country
+WHERE region IS NULL
+
+-- Check for NOT NULL values
+SELECT name
+,region
+FROM coutry
+WHERE region IS NOT NULL;
