eb1dde4b4c8327ca9a758656269c8b519a171ae1
Sammy Bruinsmsa <bruinsma.sammy@gmail.com>
2026-03-17 15:59:12 +0100

    Add aggregate functions (AVG, COUNT, MAX, MIN)

---
+++ aggregations/aggregate_functions.sql
@@ -0,0 +1,15 @@
+-- Get average population of all countries
+SELECT AVG(population)
+FROM country;
+
+-- Count total number of countries
+SELECT COUNT(*)
+FROM country;
+
+-- Get maximum population
+SELECT MAX(population)
+FROM country;
+
+-- Get minimum population
+SELECT MIN(population)
+FROM country;
