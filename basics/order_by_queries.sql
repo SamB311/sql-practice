c80601f77af3b35d15252fe91a8bf1f62a9f0bd8
Sammy Bruinsmsa <bruinsma.sammy@gmail.com>
2026-03-17 08:43:52 +0100

    Add ORDER BY practice queries

---
+++ basics/order_by_queries.sql
@@ -0,0 +1,13 @@
+-- ORDER BY practice
+
+-- Sort countries by name
+SELECT name
+,continent
+FROM country
+ORDER BY name;
+
+-- Sort by continent descending
+SELECT name
+,continent
+FROM country
+ORDER BY continent DESC;
